aa0da90414ad1cb5aa3acc6a084dfaee069536ca
nicolediaz <nicolediaz1525@gmail.com>
2017-07-07 10:36:07 -0400

    Add files via upload
    This is my entire library for what I have so far in my portfolio.

---
+++ functions.js
@@ -0,0 +1,18 @@
+function popUp() {
+  alert("Yooooooooo");
+}
+
+function changeColor(){
+  var x = document.getElementById('top-header');
+
+  x.style.color = 'white';
+
+  if (x.style.color = 'white') {
+
+
+  }
+}
+
+function changeImage() {
+  background-image: url('.../assets/volleyball.jpg') no-repeat center center;
+}
